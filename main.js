4da3d90def20b5841600003b1c16a5a821baf4d7
codemantra10 <73803525+codemantra10@users.noreply.github.com>
2021-07-11 15:45:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup(){
+    canvas=createCanvas(350,350);
+    canvas.position(550,250);
+    video=createCapture(VIDEO);
+    video.hide();
+    console.log(ml5.version);
+    imageIndentifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/TxFhP675n/",modelLoaded);
+    imageIndentifier.classify(video,gotResult);
+    }
+    function draw(){
+    image(video,0,0,350,350);
+    }
+    function modelLoaded(){
+    console.log("Great Job...you are a rockstar🤘🏽");
+    }
+    function gotResult(Error,Result){
+    if (Error) {
+    console.log(Error);    
+    }
+    else{
+    console.log(Result);
+    
+    }
+    }
